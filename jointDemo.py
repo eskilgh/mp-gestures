88daf0c7ab28b36bb81f94d60e726cab56979594
kjxlstad <jonathan.kjolstad@gmail.com>
2021-01-14 13:21:14 +0100

    Fjerning av flytte kode[C

--- jointDemo.py
+++ jointDemo.py
@@ -2,50 +2,19 @@ from cv2 import cv2
 import mediapipe as mp
 import numpy as np
 import math
-
-# Tar inn listen over ledd og indeksen til et ledd hvor vi ønsker å finne vinkelen
-# Merk at her må n være et punkt mellom 4f + 1 til og med 4f + 3
-# Dette er fordi vi kan kun måle vinkel i et nøkkelpunkt dersom det er et ledd
-def jointCurvature(lm, n):
-    # Henter koordinatene til forrige, nåværende og neste ledd
-    # Her må vi ta en dyp kopi for å passe på å ikke mutere lm
-    points = lm[n - 1 : n + 2].copy()
-
-    # Dersom punktet vi står i er det første leddet på en finger
-    # setter vi det forrige leddet til håndleddet
-    if (n - 1) % 4 == 0:
-        points[0] = lm[0]
-
-    # Trekker fra midtpunktet fra begge endepunktene for å få vektorene vi ønsker
-    u = points[0] - points[1]
-    v = points[2] - points[1]
-
-    # Finner vinkelen mellom vektorene, denne ligger mellom 0 og PI
-    a = angleBetweenVecs(u, v)
-
-    # Regner ut et tall for krumningen som vil variere mellom 0 og 1/2 PI
-    # Tilsvarende helt rett til helt krum
-
-    return math.pi - a
-
-# Inversen av dotproduktet for å finne vinkelen mellom to vektorer 
-angleBetweenVecs = lambda u, v: math.acos(np.divide(np.dot(u, v), magnitude(u) * magnitude(v)))
-
-# Magnituden til en vilkårlig dimensjonal vektor v
-# Merk at vektoren v må være itererbar
-magnitude = lambda v: math.sqrt(sum(map(lambda X: math.pow(X, 2), v)))
-
-# Tar inn listen over alle nøkkelpunkter og indeksen til en finger
-# Returnerer summen av alle vinklene på den fingeren
-fingerCurvature = lambda lm, f: sum([jointCurvature(lm, n) for n in range(4 * f + 1, 4 * f + 4)])
+from util import (
+        draw_handmarks_label,
+        draw_landmark_bbox
+ )
+from gesture_estimator import GestureEstimator
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 hands = mp_hands.Hands(
-        min_detection_confidence = 0.5, 
+        min_detection_confidence = 0.8, 
         min_tracking_confidence = 0.5,
-        max_num_hands = 1
+        max_num_hands = 2
 )
 
 cap = cv2.VideoCapture(0)
@@ -66,9 +35,24 @@ while cap.isOpened():
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             
+            draw_landmark_bbox(image, hand_landmarks)
+
+            gestEst = GestureEstimator(hand_landmarks)
+            gesture = gestEst.getGesture()
+
+            draw_handmarks_label(image, gesture, hand_landmarks) 
+            """
             # Hente hele lista med punkter
             lm = np.array([[p.x, p.y, p.z] for p in hand_landmarks.landmark])
+             
+            closedFingers = [isFingerClosed(lm, finger) for finger in range(5)]
             
+            gesture = getGesture(closedFingers)
+                   
+            draw_handmarks_label(image, gesture, hand_landmarks)
+            """
+
+            """
             # Escape symbol for clearing av terminal
             print(chr(27) + "[2J")
 
@@ -84,8 +68,10 @@ while cap.isOpened():
                 for joint in range(4 * finger + 1, 4 * finger + 4):
                     print('Joint: {}, Curvature {}'.format(joint, jointCurvature(lm, joint)))
 
-            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            """
 
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+           
     cv2.imshow('Angles of joints', image)
 
     if cv2.waitKey(5) & 0xFF == ord('q'):
